c9aba9d470d439c8cf7e4f03499a689018ebca87
Zdenek Grof <zdenek.grof@vscht.cz>
2026-07-27 08:57:06 +0200

    Update vertex/edge meta-data

--- src/graph_user.f90
+++ src/graph_user.f90
@@ -3,8 +3,8 @@
     public
 
     ! Size of vertex/edge storage space
-    integer, parameter :: VSIZE_IPAR = 3, VSIZE_RPAR = 7
-    integer, parameter :: ESIZE_IPAR = 1, ESIZE_RPAR = 3
+    integer, parameter :: VSIZE_IPAR = 4, VSIZE_RPAR = 7
+    integer, parameter :: ESIZE_IPAR = 2, ESIZE_RPAR = 4
 
     ! Maping of properties to the storeage space
     ! Vertex properties
@@ -12,20 +12,23 @@
     integer, parameter :: VPOS_TYPE  = 1 ! kind of particle
     integer, parameter :: VPOS_BC    = 2 ! boundary condition
     integer, parameter :: VPOS_COMP  = 3 ! connected component
+    integer, parameter :: VPOS_ITMP  = 4 ! temporary working space
     ! - reals
     integer, parameter :: VPOS_RADIUS = 1 ! particle radius
     integer, parameter :: VPOS_X      = 2 ! position vector (2,3,4)
     integer, parameter :: VPOS_VB     = 5 ! vertex betweenness centrality
-    integer, parameter :: VPOS_RWORK  = 6 ! temporary working space
+    integer, parameter :: VPOS_RTMP   = 6 ! temporary working space
     integer, parameter :: VPOS_C      = 7 ! concentration (potential)
 
     ! Edge properties
     ! - integers
     integer, parameter :: EPOS_TYPE = 1    ! edge type
+    integer, parameter :: EPOS_ITMP = 2    ! temporary working space
     ! - reals
     integer, parameter :: EPOS_WEIGHT = 1  ! edge weight
     integer, parameter :: EPOS_EB     = 2  ! edge betweenness centrality
     integer, parameter :: EPOS_FLOW   = 3  ! flow
+    integer, parameter :: EPOS_RTMP   = 4  ! temporary working space
 
     ! Mask for vtuio read/write operation
     integer, parameter :: &
