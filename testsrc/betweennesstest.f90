059de695e822a981644ac8ec90e9180cd3ef62a1
Zdenek Grof <grofz@vscht.cz>
2026-08-06 17:16:08 +0200

    WIP

--- testsrc/betweennesstest.f90
+++ testsrc/betweennesstest.f90
@@ -10,27 +10,15 @@ program betweeness
   use graph_mod, only : graph_t, handle_t
   use iso_fortran_env, only : dp=>real64, output_unit
   use vtuio_mod, only : vtuio_read, vtuio_write, vtuio_data_t
+  use utest_mod, only : utest_t
   implicit none (type, external)
 
   type(testsample_t) :: ts
   type(string_t), allocatable :: lines(:)
   integer :: i, k
   character(len=2) :: numstr
-  real(dp) :: time_start, time_end, time(2), mu, var
-  real(dp), allocatable :: expected_vb(:), expected_eb(:)
-  logical, allocatable :: vmask(:), emask(:)
-  type(graph_t) :: g
+  type(utest_t) :: utest
 
-  abstract interface
-    subroutine test_graph_ai(graph, is_directed, evb, eeb)
-      use graph_mod, only : graph_t, handle_t
-      use iso_fortran_env, only : dp=>real64, output_unit
-      implicit none (type, external)
-      type(graph_t), intent(inout) :: graph
-      logical, intent(in) :: is_directed
-      real(dp), allocatable, intent(out) :: evb(:), eeb(:)
-    end subroutine
-  end interface
   interface
     pure elemental function almost_equal(a,b) result(res)
       use iso_fortran_env, only : dp=>real64
@@ -46,7 +34,6 @@ program betweeness
       real(dp), intent(out) :: mu, var
     end subroutine
   end interface
-  procedure(test_graph_ai) :: test_graph1, test_graph2, test_graph3, test_graph4
   type(vtuio_data_t) :: vtudata
 
   ! betweenness tests
@@ -67,16 +54,15 @@ program betweeness
     call vtuio_write('betweennesssample'//numstr, ts%g, VTUIO_MASK, vtudata=vtudata)
     k = k+1
 
-   !print '("Vertex central betweenness: expected/got")'
-   !print '(*(g0,1x))', ts%expected_vb
-   !print '(*(g0,1x))', ts%g%vertices(1:ts%g%nvertices)%rpar(VPOS_VB)
-    print '("Passed? ",l1)', all(almost_equal(ts%g%vertices(1:ts%g%nvertices)%rpar(VPOS_VB), ts%expected_vb))
-   !print '("Edge central betweenness: expected/got")'
-   !print '(*(g0,1x))', ts%expected_eb
-   !print '(*(g0,1x))', ts%g%edges(1:ts%g%nedges)%rpar(EPOS_EB)
-    print '("Passed? ",l1)', all(almost_equal(ts%g%edges(1:ts%g%nedges)%rpar(EPOS_EB), ts%expected_eb))
+    call utest%assert_eq( .true., all(almost_equal( &
+        ts%g%vertices(1:ts%g%nvertices)%rpar(VPOS_VB), ts%expected_vb)), &
+        'Graph '//numstr//': vertex betweenness match expected values')
+    call utest%assert_eq(.true., all(almost_equal( &
+        ts%g%edges(1:ts%g%nedges)%rpar(EPOS_EB), ts%expected_eb)), &
+        'Graph '//numstr//': edge betweenness match expected values')
     print *
   end do
+  call utest%summarize()
 
 end program
 
