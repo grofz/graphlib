f63dc53e1d98907e00aff89d941fcf622ba100ac
Zdenek Grof <grofz@vscht.cz>
2026-08-23 18:05:39 +0200

    Rename unique_id to graph_id

--- src/mesh.f90
+++ src/mesh.f90
@@ -273,7 +273,7 @@
         type(graph_handle_t) :: new_handle
         do i=old_capacity+1, new_capacity0
           new_handle = graph_handle_t( &
-              id=i, version=this%get_unique_id(), type=POINT_HANDLE_TYPE)
+              id=i, version=this%get_graph_id(), type=POINT_HANDLE_TYPE)
           call this%free_phandles%enqueue(transfer(new_handle, INTEGER_MOLD))
         end do
       end block
@@ -311,7 +311,7 @@
         type(graph_handle_t) :: new_handle
         do i=old_capacity+1, new_capacity0
           new_handle = graph_handle_t( &
-              id=i, version=this%get_unique_id(), type=CELL_HANDLE_TYPE)
+              id=i, version=this%get_graph_id(), type=CELL_HANDLE_TYPE)
           call this%free_chandles%enqueue(transfer(new_handle, INTEGER_MOLD))
         end do
       end block
